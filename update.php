a6ed612ea3e3edb0db9facf5a80675804062f9df
Deepudk04 <113844143+Deepudk04@users.noreply.github.com>
2023-04-10 10:47:52 +0530

    Update update.php
    Updated

--- update.php
+++ update.php
@@ -1,3 +1,5 @@
+
+
 <?php 
 if(isset($_POST['psub']))
 {
@@ -5,17 +7,37 @@ if(isset($_POST['psub']))
    require 'vendor/autoload.php';
 
    $client = new MongoDB\Client;
-
+    $var=$_POST['pname'];
+    $var1=$_POST['age'];
+    $var2=$_POST['con'];
     $user = $client-> USER_PROFILE;
     $profUpdate= $user -> profiles;
-    $result=$profUpdate->insertOne([
-    'eMail' => $_POST['pmail'],
-    'Name' => $_POST['pname'],
-    'Age' => $_POST['age'],
-    'Contact' => $_POST['con']
-    ]);
+    $result=$profUpdate->updateOne(
+    ['eMail' => $_POST['pmail']],
+    ['$set' => ['Name' => $var]],
+    );
+    $result=$profUpdate->updateOne(
+        ['eMail' => $_POST['pmail']],
+        ['$set' => ['Age' => $var1]],
+        );
+        $result=$profUpdate->updateOne(
+            ['eMail' => $_POST['pmail']],
+            ['$set' => ['Contact' => $var2]],
+            );
+    // $result=$profUpdate->update(
+    //     array('eMail' => $_POST['pmail']),
+    //     array(
+    //         '$set' => 
+    //                   array(
+    //                          array("Name" => $_POST['pname']),
+    //                          array("Age" => $_POST['age']),
+    //                          array("Contact" => $_POST['con'])
+    //                        )
+    //     ),
+    //     array("upsert" => true) 
+    //  );
     var_dump($result);
-    if($result->getInsertedCount() === 1){
+    if($result->getModifiedCount() == 1){
         header('location:php/profile.php');
     } 
     else{
@@ -24,3 +46,5 @@ if(isset($_POST['psub']))
 }
 
 ?>
+
+
